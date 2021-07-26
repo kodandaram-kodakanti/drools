8c45d9c35643249a21651d9debf058af12cb6dcd
KodandaRamu.Kodakanti <kodandaram.kodakanti@gmail.com>
2021-07-26 19:31:35 +0530

    first drools demo

---
+++ DroolsDemoApp/src/main/java/com/drools/demo/PaymentOffer.java
@@ -0,0 +1,35 @@
+package com.drools.demo;
+
+public class PaymentOffer {
+	private String channel;
+	private int discount;
+
+	/**
+	 * @return the channel
+	 */
+	public String getChannel() {
+		return channel;
+	}
+
+	/**
+	 * @param channel the channel to set
+	 */
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+
+	/**
+	 * @return the discount
+	 */
+	public int getDiscount() {
+		return discount;
+	}
+
+	/**
+	 * @param discount the discount to set
+	 */
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+
+}
